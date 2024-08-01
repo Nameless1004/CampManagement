81b63dc502cd98bbe1dabd0a25e4f78c634164eb
Nameless1004 <jjho1029@gmail.com>
2024-08-01 20:06:43 +0900

    [feat] model 클래스들 필드 추가

--- camp-management-app/src/camp/model/Score.java
+++ camp-management-app/src/camp/model/Score.java
@@ -1,15 +1,78 @@
 package camp.model;
 
 public class Score {
-    private String scoreId;
+    private String subjectId;
+    private String subjectType;
+    private String studentId;
+    private int round;
+    private int score;
+    private String grade;
 
-    public Score(String seq) {
-        this.scoreId = seq;
+    public Score(String subjectId, String subjectType, String studentId, int round, int score, String grade) {
+        this.subjectId = subjectId;
+        this.subjectType = subjectType;
+        this.studentId = studentId;
+        this.round = round;
+        this.score = score;
+        this.grade = convertToGrade(score);
     }
 
-    // Getter
-    public String getScoreId() {
-        return scoreId;
+    private String convertToGrade(int score){
+        String grade = "";
+        switch (subjectType){
+            case "MANDATORY":
+                if( 95 <= score){
+                    grade = "A";
+                } else if(90 <= score) {
+                    grade = "B";
+                } else if(80 <= score) {
+                    grade = "C";
+                } else if(70 <= score) {
+                    grade = "D";
+                } else if(60 <= score) {
+                    grade = "F";
+                } else{
+                    grade = "N";
+                }
+                break;
+            case "CHOICE":
+                if( 90 <= score){
+                    grade = "A";
+                } else if(80 <= score) {
+                    grade = "B";
+                } else if(70 <= score) {
+                    grade = "C";
+                } else if(60 <= score) {
+                    grade = "D";
+                } else if(50 <= score) {
+                    grade = "F";
+                } else{
+                    grade = "N";
+                }
+                break;
+        }
+
+        return grade;
     }
 
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
 }
