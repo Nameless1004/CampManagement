af4f3047393d40ead329c1ee42cf926d7aaedec0
Nameless1004 <97615919+Nameless1004@users.noreply.github.com>
2024-08-01 20:08:47 +0900

    [feat] model 클래스들 필드 추가 (#14)

--- camp-management-app/src/camp/model/Student.java
+++ camp-management-app/src/camp/model/Student.java
@@ -1,12 +1,17 @@
 package camp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String studentId;
     private String studentName;
+    private List<Subject> subjectList;
 
     public Student(String seq, String studentName) {
         this.studentId = seq;
         this.studentName = studentName;
+        List<Subject> subjectList = new ArrayList<Subject>();
     }
 
     // Getter
@@ -18,4 +23,13 @@ public class Student {
         return studentName;
     }
 
+
+    // 원본은 수정 못하게 복사본을 줍니다.
+    public List<Subject> getSubjectList() {
+        return List.copyOf(subjectList);
+    }
+
+    public void addSubject(Subject subject) {
+        subjectList.add(subject);
+    }
 }
